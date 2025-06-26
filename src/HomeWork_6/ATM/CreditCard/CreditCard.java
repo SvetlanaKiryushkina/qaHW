807b792987a10b00f840237730b972820d7c3114
SvetlanaKiryushkina <svetlana198787@mail.ru>
2025-06-26 18:50:43 +0300

    Go

--- src/HomeWork_6/ATM/CreditCard/CreditCard.java
+++ src/HomeWork_6/ATM/CreditCard/CreditCard.java
@@ -4,9 +4,29 @@ public class CreditCard {
     String cardAccountNumber;
     double cardBalanceSum;
 
-    public String gerCreditCardInformation (){
+    //Создаем конструктор
+    public CreditCard(String cardAccountNumber, double cardBalanceSum) {
+        this.cardAccountNumber = cardAccountNumber;
+        this.cardBalanceSum = cardBalanceSum;
+    }
+
+    //метод вывода информации по картам
+    public String getCreditCardInformation (){
         return "Account Number is: " + cardAccountNumber + "\n"+
-                "Balance Sum is: " + cardBalanceSum + "\n";
+                "Balance Sum is: " + cardBalanceSum;
+    }
+    // метод пополнения карты
+    public void addMoney(double money){
+        this.cardBalanceSum += money;
+    }
+    //метод снятия с карты
+    public void takeMoney(double money){
+        this.cardBalanceSum -= money;
+    }
+
+    //отобразить текущий баланс
+    public double getBalance(){
+        return this.cardBalanceSum;
     }
 
 }
--- src/HomeWork_6/ATM/MyHomeWork.java
+++ src/HomeWork_6/ATM/MyHomeWork.java
@@ -8,7 +8,21 @@ public class MyHomeWork {
     }
 
     public static void createdCreditCard() {
-        CreditCard cardOne = new CreditCard();
-        System.out.println(cardOne.gerCreditCardInformation());
+        CreditCard card1 = new CreditCard("BY12345678", 125.25);
+        System.out.println(card1.getCreditCardInformation());
+
+        CreditCard card2 = new CreditCard("USD12345678", 1000.01);
+        System.out.println(card2.getCreditCardInformation());
+
+        CreditCard card3 = new CreditCard("EUR12345678", 5000.05);
+        System.out.println(card3.getCreditCardInformation());
+        System.out.println("******************************");
+
+        card1.addMoney(100);
+        System.out.println("Balance 1 card: " + card1.getBalance());
+        card2.addMoney((1000.89));
+        System.out.println("Balance 2 card: " + card2.getBalance());
+        card3.takeMoney(856.35);
+        System.out.println("Balance 3 card: " + card3.getBalance());
     }
 }
